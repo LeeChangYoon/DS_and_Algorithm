7e442cb4138d77a5e267bf9968bcf8c876a3133a
LeeChangYoon <41320453+LeeChangYoon@users.noreply.github.com>
2021-04-14 19:51:34 +0900

    Update 1244_V1.py

--- D3/1244. 최대 상금/1244_V1.py
+++ D3/1244. 최대 상금/1244_V1.py
@@ -1,3 +1,9 @@
+'''
+이 문제 같은 경우에는 처음에 생각 나는게 완전 탐색을 통한 답 구하기였다.
+그래서 생각나는게 저거 뿐이라 조건 하나하나 다 따져서 if문과 for문을 남발하여
+결국에 RunTime Error가 난 것 같다. (실제로 Test Case 10개 중 5개만 맞음)
+'''
+
 def f(data, loop, idx, res):
     if loop == 0:
         return res + data
