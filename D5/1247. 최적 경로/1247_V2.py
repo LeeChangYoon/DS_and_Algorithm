2bdd1f8a28b50b013b1fff09e4949f8ab73e5500
LeeChangYoon <41320453+LeeChangYoon@users.noreply.github.com>
2021-04-24 20:13:46 +0900

    Update 1247_V2.py

--- D5/1247. 최적 경로/1247_V2.py
+++ D5/1247. 최적 경로/1247_V2.py
@@ -1,3 +1,20 @@
+'''
+다른 사람의 풀이를 참고한 V2이다.
+아까 언급했지만, TSP & DP를 이용한 코드이다.
+사실 지금 이 순간에도 이 코드가 완전히 이해되지는 않는다.
+대충 아 TSP & DP를 이용하면 저런식으로 코드가 진행되는 지만 알 것 같다.
+차후에 TSP를 더 깊게 공부해 봐야 정확하게 알 수 있다고 말할 수 있을 것 같다.
+
+이번 문제를 통해 배운점:
+1. TSP & DP를 사용해 문제를 푸는 것
+2. float('inf') --> infinite
+3. TSP & DP를 풀 때에는 Bitmasking을 통해 풀어야 한다는 것 (이 부분이 아직까지 정확하게 이해 가지가 않는다.)
+
+더 공부해야 할 점:
+1. Bitlen은 왜 있고 왜 사용되는가?
+2. TSP & Greedy, TSP Linear, TSP & DP
+'''
+
 for test in range(1, int(input()) + 1):
     N = int(input())
     info = list(map(int, input().split()))
